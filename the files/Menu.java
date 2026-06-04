eddd91d97f89da20741edeae01f094b807042f05
ludze <zoenmichael76@gmail.com>
2026-06-04 02:05:23 -0700

    Add files via upload

---
+++ the files/Menu.java
@@ -0,0 +1,20 @@
+public class Menu {
+
+    public void showMenu() {
+
+        System.out.println();
+        System.out.println("===< School Event Manager >===");
+        System.out.println("1. Add a Participant");
+        System.out.println("2. Remove a Participant");
+        System.out.println("3. Save or load participant to file ");
+        System.out.println("4. search for participant");
+        System.out.println("5. Display all participants");
+        System.out.println("6. Display schedule ");
+        System.out.println("7. Set activity schedule for school");
+        System.out.println("8. Change Activity Schedule for school");
+        System.out.println("9. Sort by Score");
+        System.out.println("10. Report");
+        System.out.println("11. Quit");
+        System.out.print("Enter choice: ");
+    }
+}
